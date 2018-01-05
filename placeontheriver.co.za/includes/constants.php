b179e4bdbe26ec899458e774fb3efefe0303aff3
Rob Crothall <rob@crothall.co.za>
2018-01-05 11:47:32 +0200

    Initial commit

---
+++ placeontheriver.co.za/includes/constants.php
@@ -0,0 +1,37 @@
+<?php
+
+    /**
+     * Module name:	constants.php
+     *
+     * Client:			Lewis Coleman, Place on the River - info@placeontheriver.co.za
+     * Project:		Availability calendar
+     * Author:			Rob Crothall - rob@crothall.co.za - +27 83 678 5055
+     * Date Written:	2016-03-13
+     * Date changed:
+     * Copyright:		Creative Commons (CCO) license
+     *
+     * Purpose:		Global constants.
+     */
+
+    // your database's name
+    //define("DATABASE", "potr");
+    define("DATABASE", "xxxxxxxx");
+
+    // your database's password
+    define("PASSWORD", "xxxxxxxx");
+
+    // your database's server
+    define("SERVER", "www.placeontheriver.co.za");
+    //define("SERVER", "localhost");
+
+    // your database's username
+    //define("USERNAME", "potr_px");
+    define("USERNAME", "xxxxxxxx");
+    
+    // the number of months to display
+    define("DISPLAY_MONTHS", "18");
+
+    // the number of months to display side by side
+    define("MONTHS_WIDE", "2");
+
+?>
